2e23f8e8d5e2ee8efb3e71cf977e5dc337dcf0bf
francescotrudu <francescotrudu84@gmail.com>
2022-11-18 16:08:40 +0100

    Upload index.html and main.js

---
+++ scripts/main.js
@@ -0,0 +1,14 @@
+const userName = prompt ("Inserisci il tuo nome");
+const userSurname = prompt ("Inserisci il tuo cognome");
+const prefColor = prompt ("Qual è il tuo colore preferito");
+const shortYear = 22;
+const userData = userName + userSurname + prefColor + shortYear;
+console.log(userData);
+
+const userDataSpaced = `${userName} ${userSurname} ${prefColor} ${shortYear}`;
+console.log(userDataSpaced);
+
+document.getElementById("userdata").innerHTML = (userData);
+
+document.getElementById("userdata-spaced").innerHTML = (userDataSpaced);
+
